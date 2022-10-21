e148ef856f533c758aad0dd8fa19423bf8b7eb74
Ben Clark <benclark@Bens-MacBook-Pro.local>
2022-10-21 18:00:48 -0400

    menu logic

--- src/components/molecules/app-navbar/app-navbar.tsx
+++ src/components/molecules/app-navbar/app-navbar.tsx
@@ -3,24 +3,14 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import BasicMenu from '../menu';
 
 const NavBar: React.FC = () => {
     return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{"color": "#DAAD86", "backgroundColor": "#659DBD"}}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <BasicMenu />
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }} style={{"textAlign": "center"}}>
             Mobile Equine and Housecall Service
           </Typography>
---
+++ src/components/molecules/menu.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Home</MenuItem>
+        <MenuItem onClick={handleClose}>About Me</MenuItem>
+        <MenuItem onClick={handleClose}>My Clients</MenuItem>
+      </Menu>
+    </div>
+  );
+}
