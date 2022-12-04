1ca8f86a17e05c7913d6821d424c2483f60534b9
benclark1110 <benclark1110@gmail.com>
2022-12-04 18:49:12 -0500

    footer updates

--- src/components/molecules/app-footer/app-footer.tsx
+++ src/components/molecules/app-footer/app-footer.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-        <span>Place sticky footer content here.</span>
+        <span className="item">Horse & Pet Mobile Vet</span>
+        <span className="item">704-231-8993</span>
+        <span className="item">docsage@carolina.rr.com</span>
     </footer>
   );
 }
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -5,16 +5,19 @@ export const HomePage: React.FC = () => {
     return (
     <div>
       <Row className="tan">
+        {/* TO DO:  Name/Picture  */}
         <Col>
           Test 1
         </Col>
       </Row>
       <Row className="green">
+        {/* TO DO:  Services  */}
       <Col>
           Test 2
         </Col>
       </Row>
-      <Row className="red">
+      <Row className="tan">
+        {/* TO DO:  Contact/Appt info  */}
         <Col>
           Test 3
         </Col>
