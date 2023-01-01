4d395a7a6619a1e809a75e2ee4d0754cd059e36c
benclark1110 <benclark1110@gmail.com>
2022-12-31 23:28:17 -0500

    about page flex

--- src/pages/about-page.tsx
+++ src/pages/about-page.tsx
@@ -7,26 +7,26 @@ export const AboutPage: React.FunctionComponent = () => {
 
     return (
     <div>
-        <Row className="tan">
-            <Col>
-                <img className="homeImage" alt="homeImage" src={dogs} />
+        <Row className="blah">
+            <Col className="test-col blah2">
+                <img className="test-pic" src={dogs} />
             </Col>
-            <Col>
+            <Col className="test-col blah2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Col>
         </Row>
-        <Row className="tan">
-            <Col>
+        <Row className="blah">
+            <Col className="test-col o2 blah2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Col>
-            <Col>
-                <img className="homeImage" alt="homeImage" src={dogs} />
+            <Col className="o1 test-col blah2">
+                <img className="test-pic" src={dogs} />
             </Col>
         </Row>
     </div>
